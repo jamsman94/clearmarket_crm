e977229d5b79eb1f17fbc2b5e3ba8b47f2cdd506
jamsman <jamsman@163.com>
2019-02-11 10:24:49 +0800

    fix prod api error 2

--- src/common/apis/hostConfig.js
+++ src/common/apis/hostConfig.js
@@ -1,7 +1,7 @@
 const { location } = window
 const hostname = location.hostname.replace(/\..*/, '')
 const apiObj = {
-  'prod': 'https://cm-api.tophold.com/',
+  'prod': 'https://crm-api.tophold.com/',
   'test': 'http://139.196.173.182:10355/'
 }
 const crmUrl = 'http://139.196.173.182:10255/'
