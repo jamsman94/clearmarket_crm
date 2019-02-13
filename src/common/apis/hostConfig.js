1bdff89495c0f9c114caefba2342b26e94ea7e02
jamsman <jamsman@163.com>
2019-02-13 18:58:28 +0800

    fixed api

--- src/common/apis/hostConfig.js
+++ src/common/apis/hostConfig.js
@@ -2,7 +2,7 @@ const { location } = window
 const hostname = location.hostname.replace(/\..*/, '')
 const apiObj = {
   'prod': 'https://cm-crm-api.tophold.com/',
-  'test': 'http://192.168.1.44:8080/'
+  'test': 'http://139.196.173.182:10355/'
 }
 const crmUrl = 'http://139.196.173.182:10255/'
 
