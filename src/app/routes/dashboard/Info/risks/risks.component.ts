ad164a078e9236cdb6a9128ee13150a695a0646b
Min Min <jamsman94@gmail.com>
2019-03-20 16:56:36 +0800

    minor bug fix

--- src/app/routes/dashboard/Info/risks/risks.component.ts
+++ src/app/routes/dashboard/Info/risks/risks.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./risks.component.less']
 })
 export class RisksComponent implements OnInit {
+  renderList = [];
+  total = 0;
+
 
   constructor() { }
 
